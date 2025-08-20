bd8c26525f1907c38a925e843f3be1496ba5448d
hjudkins9 <113203659+hjudkins9@users.noreply.github.com>
2025-08-20 18:51:10 +0000

    implemented the flowe photos and created a jupyter notebook for implementation. Also did stuff with over the wire.

--- Keras/flower_project/root.py
+++ Keras/flower_project/root.py
@@ -7,11 +7,18 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 import pathlib
 
+
+# Downloading the flower photos from tensorflow 
 dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
 archive = tf.keras.utils.get_file(origin=dataset_url, extract=True)
-data_dir = pathlib.Path(archive).with_suffix('') / 'flower_photos'
 
-image_count = len(list(data_dir.glob('**/*.jpg')))
+# Finding the directory with the flower photos
+data_dir = pathlib.Path(archive)
+
+image_count = len(list(data_dir.glob('*/*/*.jpg')))
 print(image_count)
 
 print(data_dir)
+
+roses = list(data_dir.glob('*/roses/*'))
+PIL.Image.open(str(roses[0]))
